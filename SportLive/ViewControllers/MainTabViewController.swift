f99a8e7121161130e0e105fcd4f95e89ed5f7a6f
simonpojok <simonpojok@gmail.com>
2022-09-13 08:25:43 +0300

    created initial screens layouts

---
+++ SportLive/ViewControllers/MainTabViewController.swift
@@ -0,0 +1,62 @@
+//
+//  MainTabViewController.swift
+//  SportLive
+//
+//  Created by Simon Peter Ojok on 13/09/2022.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+
+        // Do any additional setup after loading the view.
+        configureViewControllers()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    private func configureViewControllers(){
+        let scheduledEventsViewController = templateNavigationController(
+            unselectedImage: UIImage(systemName: "calendar.badge.clock"),
+            selectedImage: UIImage(systemName: "calendar.badge.clock"),
+            rootViewController: ScheduledEventsViewController()
+        )
+        
+        let pastEventsViewController = templateNavigationController(
+            unselectedImage: UIImage(systemName: "square.fill"),
+            selectedImage: UIImage(systemName: "square.fill"),
+            rootViewController: PastEventsViewController()
+        )
+        
+        viewControllers = [pastEventsViewController, scheduledEventsViewController]
+    }
+    
+    private func templateNavigationController(
+        unselectedImage: UIImage?,
+        selectedImage: UIImage?,
+        rootViewController: UIViewController
+    ) -> UINavigationController {
+        
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        navigationController.navigationBar.tintColor = .black
+        navigationController.tabBarItem.image = unselectedImage
+        navigationController.tabBarItem.selectedImage = selectedImage
+        
+        return navigationController
+    }
+
+}
---
+++ SportLive/ViewControllers/PastEventsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PastEventsViewController.swift
+//  SportLive
+//
+//  Created by Simon Peter Ojok on 13/09/2022.
+//
+
+import UIKit
+
+class PastEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
