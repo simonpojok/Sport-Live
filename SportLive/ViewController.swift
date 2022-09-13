40f7ae6406c29bb5408c027e2220911fbd474476
simonpojok <simonpojok@gmail.com>
2022-09-13 07:24:50 +0300

    initialized the project

---
+++ SportLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportLive
+//
+//  Created by Simon Peter Ojok on 13/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
