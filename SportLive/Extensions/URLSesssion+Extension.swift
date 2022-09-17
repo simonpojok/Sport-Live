1a0661a7942c6b5f4b1927aea2878793463b3bc3
simonpojok <simonpojok@gmail.com>
2022-09-17 11:14:40 +0300

    added rxswift and rxcocoa pods

---
+++ SportLive/Extensions/URLSesssion+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  URLSesssion+Extension.swift
+//  SportLive
+//
+//  Created by Simon Peter Ojok on 17/09/2022.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct Resource<T: Decodable> {
+    let url: URL
+}
+
