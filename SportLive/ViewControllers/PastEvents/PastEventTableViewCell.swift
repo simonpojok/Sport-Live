03a34e5ae12c09e0a093c8d046e917482d24c3bb
simonpojok <simonpojok@gmail.com>
2022-09-17 12:17:48 +0300

    created table and loading data onto the table view

---
+++ SportLive/ViewControllers/PastEvents/PastEventTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PastEventTableViewCell.swift
+//  SportLive
+//
+//  Created by Simon Peter Ojok on 17/09/2022.
+//
+
+import UIKit
+
+class PastEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SportLive/ViewControllers/PastEvents/PastEventsViewController.swift
+++ SportLive/ViewControllers/PastEvents/PastEventsViewController.swift
@@ -6,22 +6,47 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class PastEventsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class PastEventsViewController: UITableViewController {
+    private let viewModel: PastEventsViewModel
+    
+    init(viewModel: PastEventsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        viewModel.loadPastEvents()
+    }
+}
 
+extension PastEventsViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let pastEventViewModel = viewModel.getEventForRowAt(indexPath.row)
+        
+        return PastEventTableViewCell()
+    }
+}
+
+extension PastEventsViewController: PastEventsViewModelDelegate {
+    func eventsDidLoad() {
+        tableView.reloadData()
+    }
 }
